452fde82a70ea648d05434af62fdde2aeace176b
Jeniffer Caroline <jeniffercaroline530@gmail.com>
2026-03-18 15:10:38 -0300

    Adicionando peimeira página HTML com Flask

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+# Importa a classe Flask (para criar o servidor)
+# e render_template (para renderizar páginas HTML)
+from flask import Flask, render_template
 
+# Cria a aplicação Flask
 app = Flask(__name__)
 
-
+# Define a rota principal ("/" = página inicial)
 @app.route("/")
 def home():
-    return "StudyTrack está funcionando!"
+    return render_template("index.html")  # Renderiza a página index.html
 
 if __name__ == "__main__":
     app.run(debug=True)
