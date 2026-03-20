d01c981d874baaf3d3a966148656ef6fa2679793
Jeniffer Caroline <jeniffercaroline530@gmail.com>
2026-03-20 13:32:56 -0300

    Implementando cadastro de matérias com formulário

--- app.py
+++ app.py
@@ -2,6 +2,12 @@
 # e render_template (para renderizar páginas HTML)
 from flask import Flask, render_template
 
+# Lista temporária para armazenar matérias (simulando banco de dados)
+from flask import request
+
+# Lista temporária para armazenar matérias (simulando banco de dados)
+materias = []
+
 # Cria a aplicação Flask
 app = Flask(__name__)
 
@@ -9,6 +15,17 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")  # Renderiza a página index.html
+# Rota para cadastro de matérias
+@app.route("/materias", methods=["GET", "POST"])
+def cadastro_materias():
 
-if __name__ == "__main__":
+# Se o usuário enviar o formulário
+    if request.method == "POST":
+        nome_materia = request.form.get("nome")
+# Adiciona na lista
+        materias.append(nome_materia)
+
+# Envia a lista para o HTML
+    return render_template("materias.html", materias=materias)       
+if __name__ == "__main__":                      
     app.run(debug=True)
