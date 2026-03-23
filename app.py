adaa1a244d5096d583a5ff7547d80385f6329c5b
Jeniffer Caroline <jeniffercaroline530@gmail.com>
2026-03-23 14:15:13 -0300

    Corrigindo duplicação com redirect após POST

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 # Importa a classe Flask (para criar o servidor)
 # e render_template (para renderizar páginas HTML)
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask import redirect, url_for
 
-# Lista temporária para armazenar matérias (simulando banco de dados)
-from flask import request
-
-# Lista temporária para armazenar matérias (simulando banco de dados)
-materias = []
+import sqlite3
 
 # Cria a aplicação Flask
 app = Flask(__name__)
+
+def conectar_db():
+    return sqlite3.connect("database.db")
+
+# Criar tabela se não existir
+def criar_tabela():
+    conn = conectar_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS materias (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+    # Execute ao iniciar
+criar_tabela()
 
 # Define a rota principal ("/" = página inicial)
 @app.route("/")
@@ -19,13 +37,28 @@ def home():
 @app.route("/materias", methods=["GET", "POST"])
 def cadastro_materias():
 
+    conn = conectar_db()
+    cursor = conn.cursor()
+
 # Se o usuário enviar o formulário
     if request.method == "POST":
         nome_materia = request.form.get("nome")
-# Adiciona na lista
-        materias.append(nome_materia)
+    
+        if nome_materia:
+            cursor.execute(
+                "INSERT INTO materias (nome) VALUES (?)", 
+                (nome_materia,)
+            )
+            conn.commit()
+            conn.close()
 
-# Envia a lista para o HTML
-    return render_template("materias.html", materias=materias)       
+            return redirect(url_for("cadastro_materias"))
+# Buscar todas as matérias 
+    cursor.execute("SELECT * FROM materias")
+    materias = cursor.fetchall()
+  
+    conn.close()
+
+    return render_template("materias.html", materias=materias)
 if __name__ == "__main__":                      
     app.run(debug=True)
